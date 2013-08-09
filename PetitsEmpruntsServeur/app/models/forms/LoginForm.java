43b5cab49ed55fc8ea95eaeb6a24f2794b4c9550
Kraft <kraft.aralya@free.fr>
2013-08-09 16:13:00 +0200

    - added check on validated in login process

--- PetitsEmpruntsServeur/app/models/forms/LoginForm.java
+++ PetitsEmpruntsServeur/app/models/forms/LoginForm.java
@@ -1,8 +1,6 @@
 package models.forms;
 
 import models.UserAccount;
-import play.data.validation.Constraints.Required;
-import play.i18n.Messages;
 
 /**
  * Classe servant à mapper les champs du formulaire de login
@@ -11,18 +9,15 @@ import play.i18n.Messages;
  */
 public class LoginForm 
 {
-	/**
-	 * Champ login
-	 */
-	@Required
+	// list of parameters
 	public String nickname;
-	
-	/**
-	 * Champ password
-	 */
-	@Required
 	public String password;
-	
+
+	// list of errors
+	public static final String ERROR_NICKNAME_IS_EMPTY = "login.error.nicknameisempty";
+	public static final String ERROR_PASSWORD_IS_EMPTY = "login.error.passwordisempty";
+	public static final String ERROR_AUTHENTICATION_FAILED = "login.error.authenticationfailed";
+	public static final String ERROR_ACCOUNT_NOT_VALIDATED = "login.error.accountnotvalidated";
 	
 	/**
 	 * Méthode de vérification des infos du formulaire de connexion. Utilisée pour vérifier si l'utilisateur existe bien et que le mot de passe est bon
@@ -30,10 +25,24 @@ public class LoginForm
 	 */
 	public String validate()
 	{
-		if(UserAccount.authenticate(nickname, password) == null)
-		{
-			return Messages.get("authentication.fail");
-		}
+		// check nickname is not empty
+		if(this.nickname.isEmpty())
+			return ERROR_NICKNAME_IS_EMPTY;
+
+		// check password is empty
+		if(this.password.isEmpty())
+			return ERROR_PASSWORD_IS_EMPTY;
+
+		// check authentication
+		// /!\ expensive (database request)
+		UserAccount userAccount = UserAccount.authenticate(nickname, password);
+		if(userAccount == null)
+			return ERROR_AUTHENTICATION_FAILED;
+
+		// check validated
+		if(!userAccount.isValidated())
+			return ERROR_ACCOUNT_NOT_VALIDATED;
+
 		return null;
 	}
 }
